f04168425b39d19390deac2073a0b57bc0f48136
gydgem <glebvyskvarko630@gmail.com>
2024-08-11 16:23:34 +0300

    add Genome/BaseGenomes/BaseGenome

--- Genome/BaseGenomes/BaseGenome.cpp
+++ Genome/BaseGenomes/BaseGenome.cpp
@@ -2,7 +2,72 @@
 // Created by gydgem on 8/11/2024.
 //
 
+
 #include "BaseGenome.h"
 
 namespace hgem {
+
+    [[maybe_unused]] std::any BaseGenome::This() {
+        throw std::runtime_error("Method \"This()\" was not implemented in the inherited class");
+    }
+
+    [[maybe_unused]] std::any BaseGenome::get() {
+        throw std::runtime_error("Method \"get()\" was not implemented in the inherited class");
+    }
+
+    [[maybe_unused]] std::any BaseGenome::get(const std::any &) {
+        throw std::runtime_error("Method \"get(const std::any &)\" was not implemented in the inherited class");
+    }
+
+    [[maybe_unused]] BaseGenome &BaseGenome::at(const std::string &) {
+        throw std::runtime_error("Method \"at(const std::string &)\" was not implemented in the inherited class");
+    }
+
+    [[maybe_unused]] BaseGenome &BaseGenome::at(u_int64_t) {
+        throw std::runtime_error("Method \"at(u_int64_t)\" was not implemented in the inherited class");
+    }
+
+    [[maybe_unused]] void BaseGenome::add(const std::string &, BaseGenome &node) {
+        throw std::runtime_error(
+                "Method \"add(const std::string &, BaseGenome &node)\" was not implemented in the inherited class");
+    }
+
+    [[maybe_unused]] void BaseGenome::add(u_int64_t, BaseGenome &node) {
+        throw std::runtime_error(
+                "Method \"add(u_int64_t, BaseGenome &node)\" was not implemented in the inherited class");
+    }
+
+    [[maybe_unused]] void BaseGenome::add(BaseGenome &node) {
+        throw std::runtime_error(
+                "Method \"add(BaseGenome &node)\" was not implemented in the inherited class");
+    }
+
+    [[maybe_unused]] void BaseGenome::add(const std::string &, const std::any &) {
+        throw std::runtime_error(
+                "Method \"add(const std::string &, const std::any &)\" was not implemented in the inherited class");
+    }
+
+    [[maybe_unused]] void BaseGenome::add(u_int64_t, const std::any &) {
+        throw std::runtime_error(
+                "Method \"add(u_int64_t, const std::any &)\" was not implemented in the inherited class");
+    }
+
+    [[maybe_unused]] void BaseGenome::erase(std::string &) {
+        throw std::runtime_error(
+                "Method \"erase(std::string &)\" was not implemented in the inherited class");
+    }
+
+    [[maybe_unused]] void BaseGenome::erase(u_int64_t) {
+        throw std::runtime_error(
+                "Method \"erase(u_int64_t)\" was not implemented in the inherited class");
+    }
+
+    [[maybe_unused]] void BaseGenome::mutation() {
+        throw std::runtime_error("Method \"mutation()\" was not implemented in the inherited class");
+    }
+
+    [[maybe_unused]] void BaseGenome::crossing(const std::any &) {
+        throw std::runtime_error("Method \"crossing(const std::any &)\" was not implemented in the inherited class");
+    }
+
 } // hgem
--- Genome/BaseGenomes/BaseGenome.h
+++ Genome/BaseGenomes/BaseGenome.h
@@ -5,10 +5,50 @@
 #ifndef GENOME_BASEGENOME_H
 #define GENOME_BASEGENOME_H
 
+#include <any>
+#include <string>
+#include <stdexcept>
+
 namespace hgem {
 
     class BaseGenome {
+    public:
+        virtual ~BaseGenome() = default;
 
+        //accesses a pointer to this class
+        [[maybe_unused]] virtual std::any This();
+
+        //returns the value of a class of type std::any
+        [[maybe_unused]] virtual std::any get();
+
+        [[maybe_unused]] virtual std::any get(const std::any&);
+
+        //access a class element
+        [[maybe_unused]] virtual BaseGenome &at(const std::string &);
+
+        [[maybe_unused]] virtual BaseGenome &at(u_int64_t);
+
+        //adds new elements to the class
+        [[maybe_unused]] virtual void add(const std::string &,BaseGenome& node);
+
+        [[maybe_unused]] virtual void add(u_int64_t ,BaseGenome& node);
+
+        [[maybe_unused]] virtual void add(BaseGenome& node);
+
+        [[maybe_unused]] virtual void add(const std::string &, const std::any&);
+
+        [[maybe_unused]] virtual void add(u_int64_t, const std::any&);
+
+        //removes elements from a class
+        [[maybe_unused]]  virtual void erase(std::string &);
+
+        [[maybe_unused]] virtual void erase(u_int64_t);
+
+        //mutates a class object
+        [[maybe_unused]] virtual void mutation();
+
+        //cross a class object
+        [[maybe_unused]] virtual void crossing(const std::any&);
     };
 
 } // hgem
