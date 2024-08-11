f7c9e2d3c574376880c6139b0ed035d5250b0526
gydgem <glebvyskvarko630@gmail.com>
2024-08-11 12:51:29 +0300

    add new class Genome/BaseGenomes/BaseGenome

---
+++ Genome/BaseGenomes/BaseGenome.cpp
@@ -0,0 +1,8 @@
+//
+// Created by gydgem on 8/11/2024.
+//
+
+#include "BaseGenome.h"
+
+namespace hgem {
+} // hgem
---
+++ Genome/BaseGenomes/BaseGenome.h
@@ -0,0 +1,16 @@
+//
+// Created by gydgem on 8/11/2024.
+//
+
+#ifndef GENOME_BASEGENOME_H
+#define GENOME_BASEGENOME_H
+
+namespace hgem {
+
+    class BaseGenome {
+
+    };
+
+} // hgem
+
+#endif //GENOME_BASEGENOME_H
